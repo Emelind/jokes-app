80f09394f498a8c942fff7e755f1f527b499f59b
Emelind <emeliejlindvall@gmail.com>
2021-11-23 12:20:54 +0100

    Add formik and yup to signin

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -1,73 +1,84 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { useContext } from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { Controller, FieldValues, SubmitErrorHandler, useForm } from 'react-hook-form';
-import { Pressable, StyleSheet } from 'react-native';
-import { Button } from 'react-native';
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, KeyboardAvoidingView, Pressable, StyleSheet, Platform } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { StackScreens } from '../helpers/types';
 import { AuthContext } from '../src/contexts/AuthContext';
 import { tokens } from '../src/translation/appStructure';
 import { translate } from '../src/translation/translation';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 const SignIn: React.FC<NativeStackScreenProps<StackScreens, "SignIn">> = (props) => {
 
     const authContext = useContext(AuthContext);
-    const [hidePassword, setHidePassword] = useState(true);
-    const toggleHidePassword = () => {
-        setHidePassword(!hidePassword)
-    }
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [disabled, setDisabled] = useState(true);
 
-    const hidePasswordText = translate(tokens.screens.signIn.HidePassword)
-    const showPasswordText = translate(tokens.screens.signIn.ShowPassword)
-
-    useEffect(() => {
-        setDisabled(email.length === 0 || password.length < 6);
-    }, [email, password]); 
+    const validationSchema = Yup.object().shape({
+        email: Yup.string()
+            .label('Email')
+            .email('Enter a valid email')
+            .required('Please enter your email address'),
+        password: Yup.string()
+            .label('Password')
+            .required('Please enter your password')
+            .min(6, 'Password must have at least 6 characters ')
+    });
 
     return (
         <View style={styles.container}>
-            <View style={styles.container}>
-
-                <View style={styles.emailContainer}>
-                    <Text style={styles.label}>{translate(tokens.screens.signIn.Email)}</Text>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={(text) => setEmail(text)}
-                    />
-                </View>
-
-                <View style={styles.passwordContainer}>
-                    <Text style={styles.label}>{translate(tokens.screens.signIn.Password)}</Text>
-                    <TextInput
-                        onChangeText={(text) => setPassword(text)}
-                        style={styles.input}
-                        secureTextEntry={hidePassword}
-                    />
-                </View>
-
-                <Pressable style={styles.toggleHidePassword} onPress={() => toggleHidePassword()}>
-                    <Text>{hidePassword ? showPasswordText : hidePasswordText}</Text>
-                </Pressable>
-
-                <Pressable
-                    style={disabled ? styles.disabledButton : styles.button}
-                    onPress={() => { authContext?.login(email, password);}}
-                    disabled={disabled}
+            <SafeAreaView style={styles.safeArea}>
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    style={styles.content}
                 >
-                    <Text style={styles.buttonLabel}>{translate(tokens.screens.signIn.SignInButtonText)}</Text>
-                </Pressable>
+                    <Formik
+                        initialValues={{ email: '', password: '' }}
+                        onSubmit={values => { authContext?.login(values.email, values.password);}}
+                        validationSchema={validationSchema}
+                    >
+                        {({ handleChange, handleBlur, handleSubmit, values, errors, isValid, isSubmitting, touched }) => (
+                            <View>
+                                <TextInput
+                                    style={styles.input}
+                                    onChangeText={handleChange('email')}
+                                    onBlur={handleBlur('email')}
+                                    value={values.email}
+                                    placeholder={translate(tokens.screens.signIn.Email)}
+                                    autoCapitalize='none'
+                                />
+                                <View style={styles.errorContainer}>
+                                    <Text style={styles.errorMessage}>{touched.email && errors.email}</Text>
+                                </View>
+                                <TextInput
+                                    style={styles.input}
+                                    onChangeText={handleChange('password')}
+                                    onBlur={handleBlur('password')}
+                                    value={values.password}
+                                    placeholder={translate(tokens.screens.signIn.Password)}
+                                    secureTextEntry
+                                    autoCapitalize='none'
+                                />
+                                <View style={styles.errorContainer}>
+                                    <Text style={styles.errorMessage}>{touched.password && errors.password}</Text>
+                                </View>
+                                <Pressable
+                                    style={isValid && !isSubmitting ? styles.button : styles.disabledButton}
+                                    disabled={!isValid || isSubmitting}
+                                    onPress={() => { handleSubmit() }}
+                                >
+                                    <Text style={styles.buttonLabel}>{translate(tokens.screens.signIn.SignInButtonText)}</Text>
+                                </Pressable>
+                            </View>
+                        )}
+                    </Formik>
 
-                <Pressable style={styles.showSignUp} onPress={() => { props.navigation.navigate("SignUp"); }}>
-                    <Text>{translate(tokens.screens.signIn.ShowSignUpButtonText)}</Text>
-                </Pressable>
+                    <Pressable style={styles.showSignUp} onPress={() => { props.navigation.navigate("SignUp"); }}>
+                        <Text>{translate(tokens.screens.signIn.ShowSignUpButtonText)}</Text>
+                    </Pressable>
 
-            </View>
+                </KeyboardAvoidingView>
+            </SafeAreaView>
         </View>
     );
 }
@@ -79,32 +90,28 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'white'
     },
-    label: {
-        marginBottom: 10
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 16,
+        paddingVertical: 32,
+    },
+    safeArea: {
+        flex: 1,
     },
     input: {
         borderWidth: 2,
         borderColor: 'black',
-        marginBottom: 10,
-        padding: 5,
+        paddingVertical: 5,
+        paddingHorizontal: 10,
         borderRadius: 10
-    },
-    emailContainer: {
-        marginTop: 200,
-        marginBottom: 20,
-        marginHorizontal: 40,
-    },
-    passwordContainer: {
-        marginBottom: 10,
-        marginHorizontal: 40,
     },
     button: {
         borderColor: 'black',
         borderWidth: 1,
         borderRadius: 10,
-        marginHorizontal: 40,
-        marginBottom: 20,
-        backgroundColor: 'lightgreen',
+        margin: 40,
+        backgroundColor: 'green',
         alignItems: 'center'
     },
     disabledButton: {
@@ -119,13 +126,18 @@ const styles = StyleSheet.create({
     buttonLabel: {
         padding: 5,
         fontWeight: 'bold',
-        fontSize: 16
-    },
-    toggleHidePassword: {
-        alignItems: 'center',
-        marginBottom: 40
+        fontSize: 16,
+        color: 'white'
     },
     showSignUp: {
         alignItems: 'center',
+    },
+    errorContainer: {
+        paddingHorizontal: 5,
+        paddingTop: 5,
+        paddingBottom: 15,
+    },
+    errorMessage: {
+        color: 'red'
     }
-})
+});
